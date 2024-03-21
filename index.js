7690cb196f4ebb07535014318c19821c9d72efa1
Abraham <abukip344@gmail.com>
2024-03-21 04:37:45 -0400

    first class functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(scuberDrivers) {
+    return scuberDrivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(scuberDrivers) {
+    return scuberDrivers.slice((scuberDrivers.length - 2), scuberDrivers.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier;
+    }
+    
+}
+
+const fareDoubler = function (fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function (fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers);
+}
+
